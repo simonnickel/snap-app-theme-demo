e06a335b098f44e5feddaa1b8b5731ce74ab552e
Simon Nickel <simonnickel@googlemail.com>
2024-05-14 10:19:39 +0200

    Add Widget extensions via Xcode

---
+++ SnapAppThemeDemo/ThemeWidget/ThemeWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  ThemeWidgetBundle.swift
+//  ThemeWidget
+//
+//  Created by Simon Nickel on 14.05.24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ThemeWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        ThemeWidget()
+    }
+}
